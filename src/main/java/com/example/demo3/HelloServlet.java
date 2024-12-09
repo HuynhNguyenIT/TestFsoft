3ff09f41e84e7161a7e0bdb7782a295963f1f2f7
HuynhNguyenIT <huynhktpm2@gmail.com>
2024-12-09 15:05:00 +0700

    2nd

--- src/main/java/com/example/demo3/HelloServlet.java
+++ src/main/java/com/example/demo3/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World gadgadgadgad!";
+        message = "Hello World gadgadgadgad sdsds!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
